aea7a7231594c9c2b1c61936ecae63cd6f1c958e
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-02-09 20:24:47 -0300

    ✨ Implement subscribe() method to Subscriber (#1)
    * ✨ Implement subscribe() method to Subscriber
    
    * 🎨 implemented this

--- asgi_websub/subscriber.py
+++ asgi_websub/subscriber.py
@@ -9,17 +9,22 @@ The conformance criteria are described in
 Reference: https://www.w3.org/TR/websub/#subscriber
 """
 import asyncio
+from typing import Any, Dict
 
 import httpx
 
-from asgi_websub.exceptions import HubNotFoundException
+from asgi_websub.exceptions import (
+    HubNotFoundException,
+    SubscriptionException,
+    TopicNotFoundException,
+)
 from asgi_websub.types import ASGIApp, DiscoverHeaders
 from asgi_websub.utils import discover_urls
 
 
 class Subscriber:
     def __init__(self, app: ASGIApp) -> None:
-        ...
+        self.topic_url = None
 
     async def discover(self, resource_url: str, headers: DiscoverHeaders = None):
         """
@@ -35,21 +40,78 @@ class Subscriber:
                     raise HubNotFoundException(f"Response error for {res.url}")
         except httpx.HTTPError as exc:
             raise HubNotFoundException(f"HTTP Exception for {exc.request.url}") from exc
-        return discover_urls(res)
+        urls = discover_urls(res)
+        self.topic_url = urls["topic"]
+        self.hub_urls = urls["hub"]
+        return urls
 
-    async def subscribe(self):
-        ...
+    async def subscribe(
+        self,
+        hub_url: str,
+        callback_url: str,
+        *,
+        topic_url: str = None,
+        lease_seconds: int = None,
+        secret: str = None,
+        params: Dict[str, Any] = None,
+        headers: Dict[str, str] = None,
+    ):
+        """
+        Allow `topic_url` for backward portability to
+        [PubSubHubbub-Core-0.4](https://pubsubhubbub.github.io/PubSubHubbub/pubsubhubbub-core-0.4.html).
+        """
+        self.topic_url = topic_url or self.topic_url
+        if self.topic_url is None:
+            raise TopicNotFoundException("Run discover() method first")
+
+        optional_data = {}
+        if lease_seconds is not None:
+            optional_data["hub.lease_seconds"] = lease_seconds
+        if secret is not None:
+            optional_data["hub.secret"] = secret
+        headers = headers or {}
+        try:
+            async with httpx.AsyncClient() as client:
+                res = await client.post(
+                    hub_url,
+                    data={
+                        "hub.callback": callback_url,
+                        "hub.mode": "subscribe",
+                        "hub.topic": self.topic_url,
+                        **optional_data,
+                    },
+                    params=params,
+                    headers={
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        **headers,
+                    },
+                )
+                if res.is_error:
+                    if res.headers.get("content-type") == "plain/text":
+                        raise SubscriptionException(res.content)
+                    raise SubscriptionException("Hub found errors in the request")
+                if res.status_code != 202:
+                    raise SubscriptionException(
+                        'Hub should send a 202 "Accepted" in case of acceptance. '
+                        "Reference: https://www.w3.org/TR/websub/#subscription-response-details"
+                    )
+        except httpx.HTTPError as exc:
+            raise HubNotFoundException(f"HTTP Exception for {exc.request.url}") from exc
 
     async def unsubscribe(self):
         ...
 
 
 async def main():
-    subs = Subscriber("haha")
-    await subs.discover(
-        "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCpdagXDNCWHnztIbwRviPXQ"
+    topic_url = "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCpdagXDNCWHnztIbwRviPXQ"
+    subs = Subscriber("")
+    urls = await subs.discover(topic_url)
+    await subs.subscribe(
+        hub_url=urls["hub"][0],
+        callback_url="https://8371f0fade04.ngrok.io",
+        topic_url=topic_url,
     )
-    # await subs.discover("haha")
+    print(urls)
 
 
 if __name__ == "__main__":
